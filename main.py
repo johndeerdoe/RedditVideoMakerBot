dfbc7dbd675524f5dba4b1c54eb86be829b23711
Phillip Farley <96608464+johndeerdoe@users.noreply.github.com>
2024-10-12 15:34:26 -0500

    bug fixes
    fixed a couple depends to make itartions work

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import asyncio
 import argparse
 import logging
+import sys
 import math
+from pathlib import Path
 from subprocess import Popen
 from sys import exit, platform as name
 from utils.console import print_step, print_substep
@@ -15,6 +17,14 @@ from utils import settings
 from utils.id import extract_id  
 import toml
 from logging.handlers import RotatingFileHandler
+from utils.ffmpeg_install import ffmpeg_install
+from utils.version import checkversion
+from utils.console import print_markdown, print_step, print_substep
+from utils.cleanup import cleanup
+from typing import NoReturn
+from prawcore import ResponseException
+
+__VERSION__ = "3.3.2"
 
 def setup_logging(log_file):
     # Set up a rotating log with a maximum size of 1MB and 3 backup files
@@ -48,7 +58,7 @@ async def main(POST_ID=None):
     settings.config = load_config()
     
     global reddit_id, reddit_object
-    reddit_object = get_subreddit_threads(POST_ID)  # Removed await
+    reddit_object = get_subreddit_threads(POST_ID)  
     reddit_id = extract_id(reddit_object)
     print_substep(f"Thread ID is {reddit_id}", style="bold blue")
     length, number_of_comments = save_text_to_mp3(reddit_object)
@@ -71,5 +81,64 @@ def run_many(times) -> None:
         asyncio.run(main())
         Popen("cls" if name == "nt" else "clear", shell=True).wait()
 
+def shutdown() -> NoReturn:
+    if "redditid" in globals():
+        print_markdown("## Clearing temp files")
+        cleanup(reddit_id)
+
+    print("Exiting...")
+    sys.exit()
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    if sys.version_info.major != 3 or sys.version_info.minor not in [10, 11]:
+        print(
+            "Hey! Congratulations, you've made it so far (which is pretty rare with no Python 3.10). Unfortunately, this program only works on Python 3.10. Please install Python 3.10 and try again."
+        )
+        sys.exit()
+    ffmpeg_install()
+    directory = Path().absolute()
+    config = settings.check_toml(
+        f"{directory}/utils/.config.template.toml", f"{directory}/config.toml"
+    )
+    config is False and sys.exit()
+
+    if (
+        not settings.config["settings"]["tts"]["tiktok_sessionid"]
+        or settings.config["settings"]["tts"]["tiktok_sessionid"] == ""
+    ) and config["settings"]["tts"]["voice_choice"] == "tiktok":
+        print_substep(
+            "TikTok voice requires a sessionid! Check our documentation on how to obtain one.",
+            "bold red",
+        )
+        sys.exit()
+    try:
+        if config["reddit"]["thread"]["post_id"]:
+            for index, post_id in enumerate(config["reddit"]["thread"]["post_id"].split("+")):
+                index += 1
+                print_step(
+                    f'on the {index}{("st" if index % 10 == 1 else ("nd" if index % 10 == 2 else ("rd" if index % 10 == 3 else "th")))} post of {len(config["reddit"]["thread"]["post_id"].split("+"))}'
+                )
+                main(post_id)
+                Popen("cls" if name == "nt" else "clear", shell=True).wait()
+        elif config["settings"]["times_to_run"]:
+            run_many(config["settings"]["times_to_run"])
+        else:
+            asyncio.run(main())
+    except KeyboardInterrupt:
+        shutdown()
+    except ResponseException:
+        print_markdown("## Invalid credentials")
+        print_markdown("Please check your credentials in the config.toml file")
+        shutdown()
+    except Exception as err:
+        config["settings"]["tts"]["tiktok_sessionid"] = "REDACTED"
+        config["settings"]["tts"]["elevenlabs_api_key"] = "REDACTED"
+        print_step(
+            f"Sorry, something went wrong with this version! Try again, and feel free to report this issue at GitHub or the Discord community.\n"
+            f"Version: {__VERSION__} \n"
+            f"Error: {err} \n"
+            f'Config: {config["settings"]}'
+        )
+        raise err
+    
+    
