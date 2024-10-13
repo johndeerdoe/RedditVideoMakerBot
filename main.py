f8d53e3de831dc1d47a1c0ab281084fbc939e728
Phillip Farley <96608464+johndeerdoe@users.noreply.github.com>
2024-10-12 21:02:04 -0500

    bugs and cleaning code
    cleaned all the imports and moved them to one file to clear clutter added a flag parser to help with future improvements
    changed default logging to info.

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python
-import asyncio
-import argparse
-import logging
-import sys
-import math
-from pathlib import Path
-from subprocess import Popen
-from sys import exit, platform as name
-from utils.console import print_step, print_substep
-from reddit.subreddit import get_subreddit_threads
-from video_creation.voices import save_text_to_mp3
-from video_creation.background import get_background_config, download_background_video, download_background_audio, chop_background
-from video_creation.final_video import make_final_video
-from video_creation.screenshot_downloader import get_screenshots_of_reddit_posts
-from utils import settings
-from utils.id import extract_id  
-import toml
-from logging.handlers import RotatingFileHandler
-from utils.ffmpeg_install import ffmpeg_install
-from utils.version import checkversion
-from utils.console import print_markdown, print_step, print_substep
-from utils.cleanup import cleanup
-from typing import NoReturn
-from prawcore import ResponseException
+from utils.main_imports import *
+
+
+args = get_args()  # Call get_args to get the parsed arguments
 
 __VERSION__ = "3.3.2"
 
@@ -43,17 +23,17 @@ def load_config():
         return toml.load(config_file)
 
 async def main(POST_ID=None):
-    # Setting up a parser to handle command-line arguments
-    parser = argparse.ArgumentParser(description="Control logging")
-    parser.add_argument('--log', action='store_true', help="Enable logging")
-    args = parser.parse_args()
+    if args.headlessoff:
+        print("Headless mode is turned off.")
+    if args.log:
+        print("Logging is enabled.")
 
     # Enable or disable logging based on the --log argument
     if args.log:
         setup_logging('app.log')  # Call the setup_logging function
         logging.info("Logging is enabled")
     else:
-        logging.disable(logging.CRITICAL)  # Disable logging by setting the level to CRITICAL
+        logging.disable(logging.info)  # Disable logging by setting the level to info
     
     settings.config = load_config()
     
@@ -90,6 +70,7 @@ def shutdown() -> NoReturn:
     sys.exit()
 
 if __name__ == "__main__":
+
     if sys.version_info.major != 3 or sys.version_info.minor not in [10, 11]:
         print(
             "Hey! Congratulations, you've made it so far (which is pretty rare with no Python 3.10). Unfortunately, this program only works on Python 3.10. Please install Python 3.10 and try again."
@@ -140,5 +121,3 @@ if __name__ == "__main__":
             f'Config: {config["settings"]}'
         )
         raise err
-    
-    
